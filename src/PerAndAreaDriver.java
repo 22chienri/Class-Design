a572275986a0b548f548457b1449a1821296dc6e
Campionnn <awesomelaptop@gmail.com>
2021-12-12 02:18:16 -0800

    fixed formatting

--- src/PerAndAreaDriver.java
+++ src/PerAndAreaDriver.java
@@ -25,7 +25,7 @@ public class PerAndAreaDriver {
             System.out.printf("Area: %s\nCircumference: %s%n", circle.areaCircle(), circle.circumCircle());
             // calculate volume
             double volume = (4.0 / 3.0) * Math.PI * circle.getRadius() * circle.getRadius() * circle.getRadius();
-            System.out.printf("Volume: %s", volume);
+            System.out.printf("Volume: %s%n", volume);
             // change values
             System.out.println("Setting radius to 12.5");
             circle.setRadius(12.5);
@@ -43,13 +43,13 @@ public class PerAndAreaDriver {
             double width = Double.parseDouble(input.nextLine());
             // create rectangle object
             PerAndArea rectangle = new PerAndArea(length, width, 0, 0);
-            System.out.printf("Area: %s\n Perimeter: %s", rectangle.areaRec(), rectangle.perRec());
+            System.out.printf("Area: %s\nPerimeter: %s%n", rectangle.areaRec(), rectangle.perRec());
             // change values
             System.out.println("Setting length to 20.5 and width to 30.5");
             rectangle.setLength(20.5);
             rectangle.setWidth(30.5);
             // print again
-            System.out.printf("Area: %s\n Perimeter: %s", rectangle.areaRec(), rectangle.perRec());
+            System.out.printf("Area: %s\nPerimeter: %s", rectangle.areaRec(), rectangle.perRec());
         }
         // if regular polygon
         else if (shape == 3) {
@@ -60,7 +60,7 @@ public class PerAndAreaDriver {
             double length = Double.parseDouble(input.nextLine());
             // create polygon object
             PerAndArea polygon = new PerAndArea(length, 0, 0, sides);
-            System.out.printf("Area: %s\nPerimeter: %s", polygon.areaPoly(), polygon.perPoly());
+            System.out.printf("Area: %s\nPerimeter: %s%n", polygon.areaPoly(), polygon.perPoly());
             // change values
             System.out.println("Setting sides to 4 and length to 20.5");
             polygon.setSides(4);
